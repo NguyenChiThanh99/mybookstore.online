96df4c94edd6d7bd920b5700311a42443da1fc6d
NguyenChiThanh99 <nguyenchithanh1999@gmail.com>
2020-06-30 16:19:03 +0700

    make it better

--- src/Components/GioHang.js
+++ src/Components/GioHang.js
@@ -210,144 +210,6 @@ export default class GioHang extends Component {
       </div>
     );
 
-    const bodyJSX2 = (
-      <div>
-        <MetaTags>
-          <title>Giỏ hàng | mybookstore.online</title>
-          <meta property="og:url" content="https://mybookstore.online/cart" />
-          <meta property="og:type" content="website" />
-          <meta
-            name="description"
-            content="Thỏa sức mua sắm qua mạng với hàng ngàn mặt hàng sách tại mybookstore.online với giá rẻ hơn và nhiều ưu đãi hấp dẫn."
-          />
-          <meta property="og:title" content="Giỏ hàng | mybookstore.online" />
-          <meta property="og:image" content={firstItemImg} />
-        </MetaTags>
-
-        {/*Path*/}
-        <div className="container py-2 px-0">
-          <NavLink to="/">
-            <p className="path float-left">Trang chủ /{"\u00A0"}</p>
-          </NavLink>
-          <p className="path textColor">Giỏ hàng</p>
-        </div>
-
-        {/*Cart*/}
-        <div className="container bg-white p-3">
-          <table className="table table-bordered table-hover">
-            <thead>
-              <tr>
-                <th>Tên</th>
-                <th style={{ whiteSpace: "nowrap" }}>Hình ảnh</th>
-                <th style={{ whiteSpace: "nowrap" }}>Đơn giá</th>
-                <th style={{ whiteSpace: "nowrap" }}>Số lượng</th>
-                <th style={{ whiteSpace: "nowrap" }}>Thành tiền</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>{this.show_cart()}</tbody>
-          </table>
-          <div className="d-flex justify-content-between">
-            <div className="d-flex justify-content-start">
-              <NavLink
-                to="/"
-                className="btn btn-danger mybtn mr-3 btn-viewmore-cart"
-              >
-                Tiếp tục mua sắm
-              </NavLink>
-              <button
-                type="button"
-                className="btn btn-danger mybtn"
-                onClick={this.handleShow2}
-              >
-                Xóa hết giỏ hàng
-              </button>
-            </div>
-            <div className="d-flex justify-content-end">
-              <div className="row">
-                <p className="ml-3">Tổng cộng: </p>
-                <p className="ml-3" style={{ color: "#EB2B3F" }}>
-                  {" "}
-                  <b>{this.currencyFormat(this.state.total.toString())} đ</b>
-                </p>
-              </div>
-
-              <NavLink
-                to="/pay"
-                type="button"
-                className={
-                  this.state.cart.length === 0
-                    ? "btn btn-secondary mybtn ml-4 btn-viewmore-cart disabled"
-                    : "btn btn-danger mybtn ml-4 btn-viewmore-cart"
-                }
-              >
-                Thanh toán
-              </NavLink>
-            </div>
-          </div>
-        </div>
-
-        {/*Suggest*/}
-        <div className="container bg-white p-3 mt-3">
-          <ul className="d-flex justify-content-center">
-            <li className="book d-flex flex-column mx-2">
-              <NavLink to="/chitietsanpham" className="product_shadow">
-                <img
-                  src={require("../images/book5.gif")}
-                  className="img-fluid align-self-center"
-                  alt="book1"
-                  width="120px"
-                />
-                <p className="bookItem2 mb-2">Tôi thấy hoa vàng trên cỏ xanh</p>
-                <p className="bookItem2" style={{ height: 21 }}>
-                  <small>Nguyễn Nhật Ánh</small>
-                </p>
-                <h6 className="bookItem textColor">
-                  <b>83.090 đ</b>
-                </h6>
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <Modal show={this.state.showModal} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Xác nhận xóa sản phẩm</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            Bạn có chắc muốn xóa cuốn{" "}
-            <b>{this.state.item === null ? "" : this.state.item.tensp}</b> khỏi
-            giỏ hàng?
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Đóng
-            </Button>
-            <Button variant="danger" onClick={this.deleteOneItem}>
-              Xóa
-            </Button>
-          </Modal.Footer>
-        </Modal>
-
-        <Modal show={this.state.showModal2} onHide={this.handleClose2}>
-          <Modal.Header closeButton>
-            <Modal.Title>Xác nhận xóa giỏ hàng</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            Bạn có chắc muốn xóa toàn bộ sản phẩm khỏi giỏ hàng?
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose2}>
-              Đóng
-            </Button>
-            <Button variant="danger" onClick={this.deleteAllItem}>
-              Xóa
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-
     const bodyJSX = (
       <div>
         <MetaTags>
@@ -372,7 +234,7 @@ export default class GioHang extends Component {
 
         <div className="container bg-white p-3">
           {/* tiêu đề */}
-          <div className="row pb-3 an-thongtin">
+          <div className="row pb-2 an-thongtin">
             <div className="col-sm-1">
               <h6 style={{ fontWeight: "bold" }}>Tên sách</h6>
             </div>
@@ -393,6 +255,7 @@ export default class GioHang extends Component {
             </div>
             <div className="col-sm-1"></div>
           </div>
+          <hr className='mt-0' />
 
           {/* nội dung */}
           {this.show_cart()}
--- src/Components/ItemCart.js
+++ src/Components/ItemCart.js
@@ -54,63 +54,123 @@ export default class ItemCart extends Component {
   render() {
     const {item} = this.state;
     return (
-      <div className="row pb-3">
-        <div className="col-sm-1 col-3">
-          <img
-            src={item.hinhanhsanpham}
-            className="img-fluid"
-            style={{ width: "70px" }}
-            alt={item.tensp}
-          />
-        </div>
-        <div className="col-sm-10 col-7 px-0">
-          <div className="row">
-            <div className="col-sm-9">
-              <NavLink to={"/product/" + item.slug} className="textColor">
-                <span>{item.tensp}</span>
-              </NavLink>
-            </div>
-            <div className="col-sm-3">
-              <div className="row">
-                <div className="col-sm-6 gia-sanpham">
-                  <span>{this.currencyFormat(item.gia.toString())} đ</span>
-                </div>
-                <div className="col-sm-6">
-                  <div
-                    className="btn-group btn-group-sm"
-                    role="group"
-                    aria-label="First group"
-                    onClick={this.giamsl}
-                  >
-                    <button type="button" className="btn btn-outline-secondary">
-                      -
-                    </button>
-                    <button type="button" className="btn btn-outline-secondary">
-                      {this.state.soluong}
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-outline-secondary"
-                      onClick={this.tangsl}
+      <div>
+        <div className="row pb-3">
+          <div className="col-sm-1 col-3">
+            <img
+              src={item.hinhanhsanpham}
+              className="img-fluid"
+              style={{ width: "70px" }}
+              alt={item.tensp}
+            />
+          </div>
+          <div className="col-sm-10 col-7 px-0">
+            <div className="row">
+              <div className="col-sm-9">
+                <NavLink to={"/product/" + item.slug} className="textColor">
+                  <span>{item.tensp}</span>
+                </NavLink>
+              </div>
+              <div className="col-sm-3">
+                <div className="row">
+                  <div className="col-sm-6">
+                    <span>{this.currencyFormat(item.gia.toString())} đ</span>
+                  </div>
+                  <div className="col-sm-6 pt-2">
+                    <div
+                      className="btn-group btn-group-sm"
+                      role="group"
+                      aria-label="First group"
                     >
-                      +
-                    </button>
+                      <button
+                        type="button"
+                        className="btn btn-outline-secondary"
+                        onClick={this.giamsl}
+                      >
+                        -
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-outline-secondary"
+                      >
+                        {this.state.soluong}
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-outline-secondary"
+                        onClick={this.tangsl}
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="col-sm-1 col-1">
+            <button
+              type="button"
+              className="btn btn-link mybtn-link"
+              onClick={() => this.getItem(item)}
+            >
+              <i className="fas fa-times" />
+            </button>
+          </div>
         </div>
-        <div className="col-sm-1 col-1">
-          <button
-            type="button"
-            className="btn btn-link mybtn-link"
-            onClick={() => this.getItem(item)}
-          >
-            <i className="fas fa-times" />
-          </button>
-        </div>
+        <hr className="mt-0" />
       </div>
+
+      // <tr>
+      //   <td>
+      //     <NavLink to={"/product/" + item.slug} className="textColor">{item.tensp}</NavLink>
+      //   </td>
+      //   <td>
+      //     <img
+      //       src={item.hinhanhsanpham}
+      //       className="img-fluid align-self-center"
+      //       alt="book1"
+      //       width="80px"
+      //     />
+      //   </td>
+      //   <td style={{ whiteSpace: "nowrap" }}>
+      //     {this.currencyFormat(item.gia.toString())} đ
+      //   </td>
+      //   <td>
+      //     <div className="btn-group" role="group" aria-label="First group">
+      //       <button
+      //         type="button"
+      //         className="btn btn-outline-secondary"
+      //         onClick={this.giamsl}
+      //       >
+      //         -
+      //       </button>
+      //       <button type="button" className="btn btn-outline-secondary">
+      //         {this.state.soluong}
+      //       </button>
+      //       <button
+      //         type="button"
+      //         className="btn btn-outline-secondary"
+      //         onClick={this.tangsl}
+      //       >
+      //         +
+      //       </button>
+      //     </div>
+      //   </td>
+      //   <td style={{ whiteSpace: "nowrap" }}>
+      //     {this.currencyFormat(thanhtien.toString())} đ
+      //   </td>
+      //   <td>
+      //     <button
+      //       type="button"
+      //       className="btn btn-danger mybtn mr-3"
+      //       style={{ whiteSpace: "nowrap" }}
+      //       onClick={() => this.getItem(item)}
+      //     >
+      //       Xóa bỏ
+      //     </button>
+      //   </td>
+      // </tr>
     );
   }
 }
