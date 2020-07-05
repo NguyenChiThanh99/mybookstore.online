7517c35bdee3072a304015d535afcf64cc5fe94b
NguyenChiThanh99 <nguyenchithanh1999@gmail.com>
2020-07-05 15:43:22 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -1,55 +1,48 @@
 import React, { Component, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import "./CSS/style.css";
-import "./CSS/mystyle.css";
-
-import routes from "./routes";
-const Header = React.lazy(() => import("./Components/Header"));
-const Footer = React.lazy(() => import("./Components/Footer"));
-const ScrollToTop = React.lazy(() => import("./Components/ScrollToTop"));
+import ScrollToTop from "./Components/ScrollToTop";
+import Header from '../src/Components/Header';
+import Footer from '../src/Components/Footer';
+import routes from './routes';
 
 export default class App extends Component {
   showContent = (routes) => {
     var result = null;
     if (routes.length > 0) {
       result = routes.map((route, index) => {
-        return (
-          <Route
-            key={index}
-            path={route.path}
-            component={route.main}
-            exact={route.exact}
-          />
-        );
+        return <Route key={index} path={route.path} component={route.main} exact={route.exact} />;
       });
     }
     return result;
-  };
+  }
 
   render() {
+    const loadingJSX = (
+      <div className="container p-3 bg-white d-flex justify-content-center">
+        <img
+          src={require("../images/loading.gif")}
+          className="img-fluid align-self-center"
+          alt="loading"
+          width="200px"
+        />
+      </div>
+    );
+
     return (
       <Router>
-        <Suspense
-          fallback={
-            <div className="container p-3 d-flex justify-content-center">
-              <img
-                src={require("./images/loading.gif")}
-                className="img-fluid align-self-center"
-                alt="loading"
-                width="200px"
-              />
-            </div>
-          }
-        >
-          <ScrollToTop>
-            <div className="container-fluid background2 px-0">
-              <Header />
+        <ScrollToTop>
+          <div className="container-fluid background2 px-0">
+            <Header />
+
+            {/* Main */}
+            <Suspense fallback={loadingJSX}>
               <Switch>{this.showContent(routes)}</Switch>
-              <Footer />
-            </div>
-          </ScrollToTop>
-        </Suspense>
+            </Suspense>
+
+            <Footer />
+          </div>
+        </ScrollToTop>
       </Router>
     );
   }
