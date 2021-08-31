9965aadfd91c48877688f37ecfa6d86d22462259
NguyenChiThanh99 <nguyenchithanh1999@gmail.com>
2021-08-31 15:36:58 +0700

    Change backend link

--- src/Components/Global.js
+++ src/Components/Global.js
@@ -1,5 +1,6 @@
 module.exports = {
-  link: "https://backend-mybookstore.herokuapp.com/",
+  // link: "https://backend-mybookstore.herokuapp.com/",
+  link: "https://mybookstore-backend.herokuapp.com/",
   linkAddress: "https://server-address-mybookstore.herokuapp.com/",
   isSignIn: false,
   isLoggedInS: false,
